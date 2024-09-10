fe8f3f58180174d57019ad5427b8d4451b8dc598
Anas-Abdelkarim <eng.anas_arouri@yahoo.com>
2024-09-10 14:44:15 +0200

    update KKT_AL_LS

--- functions/Solver_AL_LS/KKT_AL_LS.m
+++ functions/Solver_AL_LS/KKT_AL_LS.m
@@ -26,16 +26,18 @@ l =  length(equality); % l is the number of the eqautilty constraints
 
 lambda = sym('lambda',[q 1],'real');  % the dual variables for the inequaltiy constriants
 slack = sym('slack',[q 1],'real');  % the options_check variables for the inequaltiy constriants
+ro_eq = sym('ro_eq',[l 1],'real');     % the panelty for the equality constraints
 ro_ineq = sym('ro_ineq',[q 1],'real');  % tthe panelty for the inequality constraints
 gamma  = sym('gamma',[l 1],'real');     % the dual variables for the equaltiy constriants
-ro_eq = sym('ro_eq',[l 1],'real');     % the panelty for the equality constraints
+
+L = 0;
 
 
 %% the Newten step
 if isrow(decision_variables)
     decision_variables = decision_variables';
 end
-X_ = [decision_variables];
+X_ = decision_variables;
 KKT_matrix = zeros(n,n);
 KKT_vector = zeros(n,1);
 
@@ -47,7 +49,7 @@ if isstruct(f_0)
     if isfield(f_0, 'cost')
         f_0_cost = f_0.cost;
     else
-        f_0_cost =[];
+        f_0_cost = [];
     end
     error = f_0.error;
     information_matrix = f_0.omega;
@@ -73,12 +75,14 @@ for i = 1 : max(size(information_matrix))
     f_0_tatal = f_0_tatal + error{1,i}'*information_matrix*error{1,i};
 end
 
- 
+L = L + f_0_tatal;
+
 
 % inequality constraints
 if q>0
     f_i = lhs(f_i) - rhs(f_i);  % the inequality constraints in form f<=0
     f_i_slack = f_i + slack;  % slack ensures the inequality constraints are converted to equalities
+    L = L + lambda'*f_i ;
 end
 for i= 1 : q
     jac_error_ineq= jacobian(f_i_slack(i),X_);
@@ -86,7 +90,6 @@ for i= 1 : q
     KKT_matrix = KKT_matrix + jac_error_ineq'*omega_ineq*jac_error_ineq;
     Weighted_error = - (omega_ineq*f_i_slack(i)+ lambda(i));
     KKT_vector = KKT_vector + jac_error_ineq'*Weighted_error;
-
 end
 
 
@@ -94,6 +97,7 @@ end
 
 if l>0
     equality =  lhs(equality) - rhs(equality) ;  % the equality constraints in form g==0
+    L = L + gamma'*equality;
 end
 for i= 1 : l
     jac_error_eq = jacobian(equality(i),X_);
@@ -102,20 +106,20 @@ for i= 1 : l
     Weighted_error = - (omega_eq*equality(i)+ gamma(i));
     KKT_vector = KKT_vector + jac_error_eq'*Weighted_error;
 end
-
-% Lagrange function
-L = f_0_tatal + gamma'*equality + lambda'*f_i ;
-grad_L = gradient(L,X_);
  
+
 %% Export as matlab functions
 input= [decision_variables;parameters; slack; lambda; gamma; ro_ineq; ro_eq];
+
 KKT_matrix_func= sym2func(KKT_matrix,'Vars',input,option) ;
 KKT_vector_func= sym2func(KKT_vector,'Vars',input,option);
 
 f_0_func    = sym2func(f_0_tatal,'Vars',input,option); % for evaluation after finishing the calculations
-grad_L_func      = matlabFunction(grad_L,'Vars',input); % for evaluation after finishing the calculations
 
-if   ~isempty(f_i_slack)
+grad_L       = gradient(L,X_);
+grad_L_func  = matlabFunction(grad_L,'Vars',input); % for evaluation after finishing the calculations
+
+if   ~isempty(slack)
     f_i_func    = sym2func(f_i,'Vars',[decision_variables;parameters],option); % for evaluation after finishing the calculations
 else
     f_i_func=[];
@@ -131,12 +135,11 @@ end
 KKT.KKT_matrix_func            = KKT_matrix_func;
 KKT.KKT_vector_func            = KKT_vector_func;
 KKT.f_0                        = f_0         ;% symbolic varibales for the  cost function
-KKT.grad_L_func                = grad_L_func
-KKT.f_i                        = f_i_slack     ;% symbolic varibales for the  inequality function without conversion
+KKT.grad_L_func                = grad_L_func;
 KKT.equality                   = equality ;% symbolic variables for the equality==0
 KKT.f_0_func                   = f_0_func    ;
 KKT.f_i_func                   = f_i_func    ;% function of the inquality (without slack variables)
-KKT.input                      = input    ; 
+KKT.input                      = input    ;
 KKT.equality_func              = equality_func;
 KKT.decision_variables         = decision_variables;
 KKT.parameters                 = parameters        ;
